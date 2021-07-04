4eb255bedafb5766b14481f58a48604656a321e6
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-05 01:41:38 +0500

    Removed the space

--- SZRCAI/SZRCAI/Root Files/SceneDelegate.swift
+++ SZRCAI/SZRCAI/Root Files/SceneDelegate.swift
@@ -21,5 +21,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) { }
     func sceneWillResignActive(_ scene: UIScene) { }
     func sceneWillEnterForeground(_ scene: UIScene) { }
-    func sceneDidEnterBackground(_ scene: UIScene) { }
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
