375d0bee35c073b6ba8d61ba5cebabe6d9dde984
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-05 02:20:43 +0500

    removed spaces

--- SZRCAI/SZRCAI/Main code/Helpers/Extensions/UIButton.swift
+++ SZRCAI/SZRCAI/Main code/Helpers/Extensions/UIButton.swift
@@ -5,7 +5,7 @@ extension UIButton {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
         animation.timingFunction = .init(name: .linear)
         animation.duration = 0.5
-        animation.values = [-15, 15, -15, 15, -5, 5, -2.5, 2.5, 0]
+        animation.values = [-15,15,-15,15,-5,5,-2.5,2.5,0]
         layer.add(animation, forKey: "shake")
     }
 }
