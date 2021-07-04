26a2d6c35bf12a3921dfda6d3bc832ee812dabae
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-05 02:19:14 +0500

    typealias UIApp

--- SZRCAI/SZRCAI/Main code/Helpers/alertAuthorizedWhenInUse.swift
+++ SZRCAI/SZRCAI/Main code/Helpers/alertAuthorizedWhenInUse.swift
@@ -1,14 +1,16 @@
 import UIKit
 
+typealias UIApp = UIApplication
+
 func alertAuthorizedWhenInUse() -> UIAlertController {
     let alert = UIAlertController(title: "You have banned location tracking", message: "To resume tracking location, grant the app access to geolocation in your iPhone settings\n(Privacy > Location Services)", preferredStyle: .alert)
     
     alert.addAction(.init(title: "Close", style: .cancel))
     alert.addAction(.init(title: "Settings", style: .default, handler: { _ in
-        let settings = UIApplication.openSettingsURLString
+        let settings = UIApp.openSettingsURLString
         if let id = Bundle.main.bundleIdentifier,
            let url = URL(string: "\(settings)&path=LOCATION/\(id)") {
-            UIApplication.shared.open(url)
+            UIApp.shared.open(url)
         }
     }))
     return alert
